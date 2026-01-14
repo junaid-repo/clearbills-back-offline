755a4e4e314b5489fc53f1a3e4df6549dc0ede77
junaid-repo <junaidraza3001@gmail.com>
2026-01-14 14:43:22 +0530

    small push

---
+++ shop/src/main/java/com/management/shop/controller/ShutdownController.java
@@ -0,0 +1,23 @@
+package com.management.shop.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShutdownController {
+    @CrossOrigin(origins = {"*"})
+    @PostMapping({"/shutdown"})
+    public void shutdownApp() {
+        (new Thread(() -> {
+            try {
+                Thread.sleep(500L);
+                System.out.println("Shutting down application...");
+                System.exit(0);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        })).start();
+    }
+}
